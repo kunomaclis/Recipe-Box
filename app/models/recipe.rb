25586dbb59fedc8635288425895a09a10d79e53f
Dayne Beck & Krista Starr & Mitchell Hendee & Regina DeAngelis <pair+dbeck28.kristastarr.kunomaclis.reginad1@devbootcamp.com>
2017-08-09 12:29:34 -0500

    associations in a stable place

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_many :comments
+  has_many :ratings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :comments
+  has_many :favorites
+  has_many :recipes, through: :favorites
+  has_many :recipes
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
