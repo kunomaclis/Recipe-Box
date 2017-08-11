3c416e6a751cd31d4e50a4dfc0b843881d23cec4
[Mitchell Hendee] <mitchellhendee@gmail.com>
2017-08-10 21:57:39 -0500

    finalize box shadows and improve devise error messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,20 +1,20 @@
 class RecipesController < ApplicationController
   include RecipesHelper
-
+  
   def show
     @recipe = Recipe.find(params[:id])
   end
-
+  
   def new
     @recipe = Recipe.new
   end
-
+  
   def edit
     @recipe = Recipe.find(params[:id])
   end
-
-
-
+  
+  
+  
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
@@ -23,15 +23,15 @@ class RecipesController < ApplicationController
       @ingredient = Ingredient.find_or_create_by(name: stat[:ingredient])
       @metric = Metric.find(stat[:metric])
       @amount = Amount.find_or_create_by(number: stat[:amount])
-
-
-
+      
+      
+      
       if @recipe.save
         @recipe.recipe_ingredients.create(
-          ingredient: @ingredient,
-          metric: @metric,
-          amount: @amount
-          )
+        ingredient: @ingredient,
+        metric: @metric,
+        amount: @amount
+        )
       end
     end
     if @recipe.save
@@ -40,31 +40,30 @@ class RecipesController < ApplicationController
       render :action => 'new'
     end
   end
-
-
-
+  
+  
+  
   def update
     @recipe = Recipe.find(params[:id])
-
+    
     if @recipe.update(params[:recipe])
       redirect_to @recipe
     else
       render :action => 'edit'
     end
   end
-
+  
   def destroy
     @recipe = Recipe.find(params[:id])
-    binding.pry
     @recipe.destroy
     redirect_to current_user
   end
-
+  
   def toggle_favorite
     @recipe = Recipe.find(params[:id])
     @user = current_user
     # redirect_to @user if owns_recipe(@user, @recipe)
-     # With a param that says Hey You made This Stop
+    # With a param that says Hey You made This Stop
     # Check if user has favored the recipe before
     if found_favorite(@user, @recipe)
       # remove
@@ -76,15 +75,15 @@ class RecipesController < ApplicationController
       redirect_to @recipe
     end
   end
-
-
+  
+  
   private
   def recipe_params
     params.require(:recipe).permit(:title, :summary, :difficulty, :prep_time, :instructions, :category_id, :user_id)
   end
-
+  
   def recipe_ingredients_params
     params.require(:recipe_ingredients).permit(:ingredient, :amount, :metric)
   end
-
+  
 end
